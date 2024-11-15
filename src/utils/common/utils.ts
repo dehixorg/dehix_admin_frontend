ed58876ee82f10e3d598f87a3bfc11479ffe94f4
AyushGiri190 <giriayush190@gmail.com>
2024-11-15 21:52:38 +0530

    Added icon for status

--- src/utils/common/utils.ts
+++ src/utils/common/utils.ts
@@ -1,3 +1,5 @@
+import { BadgePlus, BadgeCheck, BadgeAlert, RotateCcw } from "lucide-react";
+import React from "react";
 export const getStatusBadge = (status: string | undefined) => {
     switch (status?.toLowerCase()) {
         case "accepted":
@@ -16,5 +18,22 @@ export const getStatusBadge = (status: string | undefined) => {
             return "bg-gray-500 hover:bg-gray-600";
         default:
             return  "bg-gray-500 hover:bg-gray-600" ;
+    }
+  };
+
+  export const getStatusIcon = (status: string): JSX.Element | null => {
+    switch (status?.toLowerCase()) {
+      case "added":
+        return React.createElement(BadgePlus, { className: "text-yellow-500" });
+      case "verified":
+        return React.createElement(BadgeCheck, { className: "text-green-500" });
+      case "rejected":
+        return React.createElement(BadgeAlert, { className: "text-red-500" });
+      case "reapplied":
+        return React.createElement(RotateCcw, { className: "text-gray-500" });
+      case "pending":
+        return React.createElement(BadgePlus, { className: "text-yellow-500" });
+      default:
+        return null;
     }
   };
