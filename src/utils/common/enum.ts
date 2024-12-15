e6b88797a0437dc7ef15b32cba1657d49bb38f70
AyushGiri190 <giriayush190@gmail.com>
2024-12-15 18:39:28 +0530

    reverted

--- src/utils/common/enum.ts
+++ src/utils/common/enum.ts
@@ -22,8 +22,8 @@ export const Admin_Schema_Selecter = [
   { value: "Super_Admin", label: "Super Admin" },
 ];
 export enum statusType {
-  active = "ACTIVE",
-  inactive = "INACTIVE",
+  active = "active",
+  inactive = "inactive",
 }
 
 export enum Api_Methods {
