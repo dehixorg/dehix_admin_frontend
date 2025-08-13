68bd4cb5ccbf7a8291861e2d917a7e3091765541
Mohd Saad Siddiqui <158738867+Saad77codecutter@users.noreply.github.com>
2025-08-13 16:25:41 +0530

    Update addskill.tsx

--- src/components/skill/addskill.tsx
+++ src/components/skill/addskill.tsx
@@ -76,7 +76,6 @@ const AddSkill: React.FC<CustomTableChildComponentsProps> = ({ refetch }) => {
 
     try {
       const skillDataWithUser = { ...data, createdBy: currentUser.type.toUpperCase(), createdById: currentUserId };
-      console.log(skillDataWithUser)
       const response = await apiHelperService.createSkill(skillDataWithUser);
       if(response.success) {
         toast({
