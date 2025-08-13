1951d437a23dab7f39c4d228d19186eb225adb49
Mohd Saad Siddiqui <158738867+Saad77codecutter@users.noreply.github.com>
2025-08-13 16:14:50 +0530

    Update page.tsx

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -31,7 +31,6 @@ export default function Login() {
     try {
       const userCredential: UserCredential = await loginUser(email, pass);
       const { user, claims } = await getUserData(userCredential);
-      console.log(claims)
       if (claims.type !== 'admin' && claims.type !=='super admin') {
         setError("You are not admin");
         return;
